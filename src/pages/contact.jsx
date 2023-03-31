bd5c0bd52ef4593ad044ce685e8b05ca72961e97
Tolulope Arinola <78857611+amazingrace17@users.noreply.github.com>
2023-03-31 08:42:00 +0100

    contact

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,16 +2,12 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
-import Button from 'react-bootstrap/Button';
 export const Contact = () => {
 
   return  <Container fluid>
   <Row >
-  <h2  className='mb-4 pt-4'>Have you completed the second stage of the shopping task? i.e shoppped on Tife store? </h2> 
-  <h2> If Yes, you have successfully completed the demonstration. Thank you</h2>
-   
-   <h2 > If No,  please click the button to move to the second stage</h2>
-   <Button vaiant="Success" size="md" href='https://tifestore.netlify.app/' className='m-3 p-3'>  Stage 2 </Button>
+  <h2 className="p-3"> Thank you for shopping with Femtech Store</h2>
+ 
   </Row>
 </Container>
 };
